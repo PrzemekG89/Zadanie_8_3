72b59301088dbedad219cc08affb7b7b78497f8e
Przemek Gałdys <galdys.przemek@gmail.com>
2017-10-25 16:30:13 +0200

    zmiana pliku script.js do zadania 8.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 // scripts.js
-
+/*
+to jest mój 
+komentarz blokowy
+*/
+//zadanie 8.4
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+//alert('Hello, ' + name);
 console.log('Hello, ' + name);
